7bd1c342cda751014c7d1505911f47b605e9b54d
selka <gled907@gmail.com>
2023-11-23 15:40:44 +0100

    pagination display text change

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -13,7 +13,7 @@
 <template>
     <div class="flex select-none items-center justify-between w-full max-w-[400px] px-5">
         <nuxt-icon @click="back" :class="`text-4xl ${currPage > 1 ? 'text-primary cursor-pointer' : 'text-gray-normal'}`" name="arrow"/>
-        <div class="text-primary text-2xl">{{currPage}}</div>
+        <div class="text-primary text-2xl">Faqe {{currPage}} nga {{ pagesNo }}</div>
         <nuxt-icon @click="next" name="arrow" :class="`text-4xl [&>*:first-child]:rotate-180 ${currPage < pagesNo ? 'text-primary cursor-pointer' : 'text-gray-normal'}`"/>
     </div>
 </template>
