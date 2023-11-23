c61181919516e03ebf1d055194c86e07a5da22d9
selka <gled907@gmail.com>
2023-11-23 14:38:27 +0100

    changed home bg

--- components/sections/HomeIntro.vue
+++ components/sections/HomeIntro.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="['relative flex flex-col max-h-[750px]']" :style="{height: height}">
-        <nuxt-img format="webp"  width="1920" height="1080" class="w-full h-full object-cover absolute top-0 left-0 z-0 max-h-[750px]" :style="{'height': height}"  src="/images/bg-image.png" alt="Background Image"/>
+        <nuxt-img format="webp"  width="1920" height="1080" class="w-full h-full object-cover absolute top-0 left-0 z-0 max-h-[750px]" :style="{'height': height}"  src="/images/bg4.png" alt="Background Image"/>
         <div class="bg-gradient-to-r from-black/20 absolute w-full h-full left-0 top-0 z-0" :style="{'max-height': height}" />
         <div class="flex flex-1 relative z-10 justify-center px-3.5 sm:px-5 lg:px-10">
             <div class="flex flex-col max-w-page w-full mt-5 sm:mt-4">
@@ -25,7 +25,7 @@
 import TravelSearch from "~/components/TravelSearch.vue";
 import Reviews from "~/components/Reviews.vue";
 
-const height = ref('100dvh')
+const height = ref('100dvh');
 
 onMounted(() => {
     if(process.client){
